e1425d18f33a14a74ad1b1241e13b2a1dc865e62
Nikita <nikitadhir98@gmail.com>
2021-09-20 22:20:24 +0530

    Updated code with book keeping and cron

--- automation.sh
+++ automation.sh
@@ -74,3 +74,35 @@ cp $tarfile \
 s3://${s3_bucket}/${myname}-httpd-logs-${timestamp}.tar
 
 
+echo "Bookkeeping begins....."
+inventory_file="/var/www/html/inventory.html"
+if [ -f "$inventory_file" ]; then
+ echo -e "$inventory_file file already exists."
+else
+ echo -e "Creating $inventory_file placeholder file..."
+ echo "<b>Log Type &nbsp;&nbsp;&nbsp;&nbsp; Date Created &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Type &nbsp;&nbsp;&nbsp;&nbsp; Size</b><br>" > $inventory_file
+fi
+ archive_size=`du -hs /tmp/$myname-httpd-logs-$timestamp.tar | awk  '{print $1}'`
+echo "<br>httpd-logs &nbsp;&nbsp;&nbsp; ${timestamp} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; tar &nbsp;&nbsp;&nbsp; ${archive_size}" >> $inventory_file
+echo -e "Inventory File Written with Archive Logs details."
+
+
+echo "Scheduling Daily Execution..."
+cronfile='/etc/cron.d/automation'
+
+if [ ! -f  $cronfile ]
+then
+ echo "$cronfile will be created for daily script execution"
+ (sudo echo '0 0 * * 0-6 root /root/Automation_Project/automation.sh >> /root/Automation_Project/cron.log 2>/root/Automation_Project/cron.err' > $cronfile)
+ created=$?
+ if [ ${created} -gt 0 ]
+ then
+  echo "Failed to create cron file $cronfile"
+  exit 6
+ else
+  echo "cron entry created in $cronfile !"
+ fi
+else
+ echo "$cronfile exists!"
+fi
+echo -e "Daily Execution scheduled!\n"
